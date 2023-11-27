00369ec9c724a7464b89bdef8ac8fef37181636d
Aporial <klaviatyra101@gmail.com>
2023-11-27 21:32:12 +0200

    modified:   main.py modified:   main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import flet as ft
 
+
 def main(page: ft.Page):
-    text = ft.Text(value="Hello World", color='Green')
+    text = ft.Text(value="Helljjjjjjjjjo Wold", color='Green')
     page.controls.append(text)
     page.update()
-    
+
 
 ft.app(target=main)
