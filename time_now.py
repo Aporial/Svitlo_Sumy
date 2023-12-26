afc739532fc6ae078f716b3b98c1b3f183da0f64
Aporia <klaviatyra101@gmail.com>
2023-12-26 09:24:28 +0200

    add more .py files with code

---
+++ time_now.py
@@ -0,0 +1,116 @@
+import flet as ft
+
+
+time_now_1 = ft.Container(
+    visible=False,
+    shadow=ft.BoxShadow(
+        blur_radius=2,
+        blur_style=ft.ShadowBlurStyle.NORMAL,
+        offset=ft.Offset(0, 2),
+        color=ft.colors.BLACK54
+    ),
+    bgcolor='#ffcc66',
+    border_radius=5,
+    padding=10,
+    content=ft.Text(
+        size=21,
+        weight='w500',
+        color=ft.colors.BLACK,
+        font_family="Golos Text"
+    )
+)
+
+time_now_2 = ft.Container(
+    visible=False,
+    shadow=ft.BoxShadow(
+        blur_radius=2,
+        blur_style=ft.ShadowBlurStyle.NORMAL,
+        offset=ft.Offset(0, 2),
+        color=ft.colors.BLACK54
+    ),
+    bgcolor='#ffcc66',
+    border_radius=5,
+    padding=10,
+    content=ft.Text(
+            size=21,
+            weight='w500',
+            color=ft.colors.BLACK,
+            font_family="Golos Text"
+    )
+)
+
+time_now_3 = ft.Container(
+    visible=False,
+    shadow=ft.BoxShadow(
+        blur_radius=2,
+        blur_style=ft.ShadowBlurStyle.NORMAL,
+        offset=ft.Offset(0, 2),
+        color=ft.colors.BLACK54
+    ),
+    bgcolor='#ffcc66',
+    border_radius=5,
+    padding=10,
+    content=ft.Text(
+            size=21,
+            weight='w500',
+            color=ft.colors.BLACK,
+            font_family="Golos Text"
+    )
+)
+
+time_now_4 = ft.Container(
+    visible=False,
+    shadow=ft.BoxShadow(
+        blur_radius=2,
+        blur_style=ft.ShadowBlurStyle.NORMAL,
+        offset=ft.Offset(0, 2),
+        color=ft.colors.BLACK54
+    ),
+    bgcolor='#ffcc66',
+    border_radius=5,
+    padding=10,
+    content=ft.Text(
+            size=21,
+            weight='w500',
+            color=ft.colors.BLACK,
+            font_family="Golos Text"
+    )
+)
+
+time_now_5 = ft.Container(
+    visible=False,
+    shadow=ft.BoxShadow(
+        blur_radius=2,
+        blur_style=ft.ShadowBlurStyle.NORMAL,
+        offset=ft.Offset(0, 2),
+        color=ft.colors.BLACK54
+    ),
+    bgcolor='#ffcc66',
+    border_radius=5,
+    padding=10,
+    content=ft.Text(
+            size=21,
+            weight='w500',
+            color=ft.colors.BLACK,
+            font_family="Golos Text"
+    )
+)
+
+time_now_6 = ft.Container(
+    visible=False,
+    shadow=ft.BoxShadow(
+        blur_radius=2,
+        blur_style=ft.ShadowBlurStyle.NORMAL,
+        offset=ft.Offset(0, 2),
+        color=ft.colors.BLACK54
+    ),
+    bgcolor='#ffcc66',
+    border_radius=5,
+    padding=10,
+    content=ft.Text(
+            size=21,
+            weight='w500',
+            color=ft.colors.BLACK,
+            font_family="Golos Text"
+    )
+)
---
+++ time_tomorrow.py
@@ -0,0 +1,115 @@
+import flet as ft
+
+time_tomorrow_1 = ft.Container(
+    visible=False,
+    shadow=ft.BoxShadow(
+        blur_radius=2,
+        blur_style=ft.ShadowBlurStyle.NORMAL,
+        offset=ft.Offset(0, 2),
+        color=ft.colors.BLACK54
+    ),
+    bgcolor='#ffcc66',
+    border_radius=5,
+    padding=10,
+    content=ft.Text(
+            size=21,
+            weight='w500',
+            color=ft.colors.BLACK,
+            font_family="Golos Text"
+    )
+)
+
+time_tomorrow_2 = ft.Container(
+    visible=False,
+    shadow=ft.BoxShadow(
+        blur_radius=2,
+        blur_style=ft.ShadowBlurStyle.NORMAL,
+        offset=ft.Offset(0, 2),
+        color=ft.colors.BLACK54
+    ),
+    bgcolor='#ffcc66',
+    border_radius=5,
+    padding=10,
+    content=ft.Text(
+            size=21,
+            weight='w500',
+            color=ft.colors.BLACK,
+            font_family="Golos Text"
+    )
+)
+
+time_tomorrow_3 = ft.Container(
+    visible=False,
+    shadow=ft.BoxShadow(
+        blur_radius=2,
+        blur_style=ft.ShadowBlurStyle.NORMAL,
+        offset=ft.Offset(0, 2),
+        color=ft.colors.BLACK54
+    ),
+    bgcolor='#ffcc66',
+    border_radius=5,
+    padding=10,
+    content=ft.Text(
+            size=21,
+            weight='w500',
+            color=ft.colors.BLACK,
+            font_family="Golos Text"
+    )
+)
+
+time_tomorrow_4 = ft.Container(
+    visible=False,
+    shadow=ft.BoxShadow(
+        blur_radius=2,
+        blur_style=ft.ShadowBlurStyle.NORMAL,
+        offset=ft.Offset(0, 2),
+        color=ft.colors.BLACK54
+    ),
+    bgcolor='#ffcc66',
+    border_radius=5,
+    padding=10,
+    content=ft.Text(
+            size=21,
+            weight='w500',
+            color=ft.colors.BLACK,
+            font_family="Golos Text"
+    )
+)
+
+time_tomorrow_5 = ft.Container(
+    visible=False,
+    shadow=ft.BoxShadow(
+        blur_radius=2,
+        blur_style=ft.ShadowBlurStyle.NORMAL,
+        offset=ft.Offset(0, 2),
+        color=ft.colors.BLACK54
+    ),
+    bgcolor='#ffcc66',
+    border_radius=5,
+    padding=10,
+    content=ft.Text(
+            size=21,
+            weight='w500',
+            color=ft.colors.BLACK,
+            font_family="Golos Text"
+    )
+)
+
+time_tomorrow_6 = ft.Container(
+    visible=False,
+    shadow=ft.BoxShadow(
+        blur_radius=2,
+        blur_style=ft.ShadowBlurStyle.NORMAL,
+        offset=ft.Offset(0, 2),
+        color=ft.colors.BLACK54
+    ),
+    bgcolor='#ffcc66',
+    border_radius=5,
+    padding=10,
+    content=ft.Text(
+            size=21,
+            weight='w500',
+            color=ft.colors.BLACK,
+            font_family="Golos Text"
+    )
+)
