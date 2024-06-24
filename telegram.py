e23238918f53a8d899624f7aec7177e201d73469
Aporial <klaviatyra101@gmail.com>
2024-06-24 14:32:40 +0300

    modified:   telegram.py

--- telegram.py
+++ telegram.py
@@ -2075,7 +2075,7 @@ def main(page: Page):
                 line_1,
                 line_2,
                 line_3,
-                line_4,
+                # line_4,
                 # line_5,
                 # line_6
             ]
