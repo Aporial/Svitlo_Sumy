866d7294f806055bfff049bbc606f38e9c48bfaa
Aporial <klaviatyra101@gmail.com>
2024-07-08 23:10:51 +0300

    modified:   database/database.json 	modified:   for_telegram/day.py 	modified:   telegram.py

--- telegram.py
+++ telegram.py
@@ -3944,9 +3944,9 @@ def main(page: Page):
                     rows=[
                         line_1,
                         line_2,
-                        # line_3,
-                        # line_4,
-                        # line_5,
+                        line_3,
+                        line_4,
+                        line_5,
                         # line_6,
                         # line_7,
                         # line_8,
