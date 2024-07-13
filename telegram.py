e3707933b202b054945cc043e38d4fc2c96dd3ba
Aporia <klaviatyra101@gmail.com>
2024-07-13 14:55:59 +0300

    modified:   database/database.json 	modified:   telegram.py

--- telegram.py
+++ telegram.py
@@ -3944,7 +3944,7 @@ def main(page: Page):
                     rows=[
                         line_1,
                         line_2,
-                        line_3,
+                        # line_3,
                         # line_4,
                         # line_5,
                         # line_6,
