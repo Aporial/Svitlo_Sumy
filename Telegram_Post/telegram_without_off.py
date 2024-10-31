596ae891efd54e81cfa14f714cac9e2a4b5eb360
Aporia <klaviatyra101@gmail.com>
2024-10-31 17:30:46 +0200

    modified:   Telegram_Post/day.py 	modified:   Telegram_Post/telegram_without_off.py 	modified:   database/database.json

--- Telegram_Post/telegram_without_off.py
+++ Telegram_Post/telegram_without_off.py
@@ -5,9 +5,9 @@ from day import today
 
 def main(page: Page):
 
-    day = datetime.now().strftime(f"{today}.%m.%Y")
+    # day = datetime.now().strftime(f"{today}.%m.%Y")
 
-    # day = "09.10.2024"
+    day = "01.10.2024"
 
     day_container = Container(
         blur=10,
