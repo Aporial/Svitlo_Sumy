9209063a222ae1afad7b812ff99939aa67441569
Aporia <klaviatyra101@gmail.com>
2024-11-17 09:21:05 +0200

    modified:   Telegram_Post/instagram_without_off.py 	new file:   Telegram_Post/telegram_avaria.py 	modified:   Telegram_Post/telegram_without_off.py

--- Telegram_Post/telegram_avaria.py
+++ Telegram_Post/telegram_avaria.py
@@ -1,8 +1,12 @@
 from flet import *
 from datetime import datetime
+from day import today
 
 
 def main(page: Page):
+
+    # day = datetime.now().strftime(f"{today}.%m.%Y")
+
     day = "17.11.2024"
 
     day_container = Container(
@@ -12,7 +16,7 @@ def main(page: Page):
         border_radius=15,
         content=Text(
             f" {day} ",
-            size=60,
+            size=35,
             weight='w500',
             color='#ffcc66',
             font_family="Golos Text"
@@ -23,14 +27,14 @@ def main(page: Page):
         content=Image(
             src=f"/icon.png",
             gapless_playback=True,
-            height=500,
-            width=500,
+            height=150,
+            width=150,
         )
     )
 
     title = Text(
-        'Без відключень на',
-        size=60,
+        'Аварійні відключення',
+        size=35,
         weight='w500',
         color=colors.BLACK,
         font_family="Golos Text"
@@ -39,12 +43,15 @@ def main(page: Page):
     head = Row(
         alignment='center',
         controls=[
+            logo,
             title,
-            day_container
+            # day_container
         ]
     )
 
     main_container = Container(
+        # alignment='center',
+        # horizontal_alignment='center',
         expand=True,
         gradient=LinearGradient(
             begin=alignment.top_right,
@@ -54,11 +61,7 @@ def main(page: Page):
         padding=15,
         content=Column(
             alignment='center',
-            horizontal_alignment='center',
-            # alignment=MainAxisAlignment.SPACE_AROUND,
-            spacing=90,
             controls=[
-                logo,
                 head
             ]
         )
@@ -69,13 +72,13 @@ def main(page: Page):
     }
     page.title = 'Svitlo Sumy'
     page.theme_mode = ThemeMode.LIGHT
-    page.window.height = 1000
-    page.window.width = 1000
+    page.window.height = 240
+    page.window.width = 760
     page.padding = 0
     page.vertical_alignment = 'center'
     page.horizontal_alignment = 'center'
     page.window.center()
-    page.window_resizable = False
+    # page.window_resizable = False
     page.window.title_bar_hidden = True
     page.window.title_bar_buttons_hidden = True
     page.add(main_container)
